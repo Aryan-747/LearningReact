6ba6814d2abac706448508988f90bb86705b2ce0
Aryan Salaria <arayn.32@gmail.com>
2024-11-12 21:36:14 +0530

    Removed handleNameChange

--- src/App.js
+++ src/App.js
@@ -3,18 +3,9 @@ import Header from './Header';
 
 function App() {
 
-  const handleNameChange = () => {
-    const names = ['Davey','Bob','Kevin De Bruyne','Mikel','Mike'];
-    const int = Math.floor(Math.random()*5);
-    return names[int];
-  }
-
-
   return (
     <div className="App">
           <Header/>
-           Hello {handleNameChange()}!
-        
     </div>
   );
 }

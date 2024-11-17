218f8f9b8466ca9e078e552786361bdafc424e8b
Aryan Salaria <arayn.32@gmail.com>
2024-11-17 23:12:20 +0530

    removed Counter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Header from './Header';
 import Content2 from './Content2';
 import Footer from './Footer';
-import Counter from './Counter';
 
 function App() {
 
@@ -10,7 +9,6 @@ function App() {
           <Header title = "Grocery List"/>
           <Content2/>
           <Footer/>
-          <Counter/>
     </div>
   );
 }

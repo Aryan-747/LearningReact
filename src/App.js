d47799b37c2250c84a27e24f2c4330f3a59e37a0
Aryan Salaria <arayn.32@gmail.com>
2024-11-17 23:05:55 +0530

    removed randompsgen from app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Header from './Header';
 import Content2 from './Content2';
 import Footer from './Footer';
-import RandomPassword from './Randomps';
 
 function App() {
 
@@ -10,7 +9,6 @@ function App() {
           <Header title = "Grocery List"/>
           <Content2/>
           <Footer/>
-          <RandomPassword/>
     </div>
   );
 }
